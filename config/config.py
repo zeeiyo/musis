37bfba8800ea0f9b8a581ed849f8dd3124a40744
dim <147292243+erslry@users.noreply.github.com>
2023-11-04 09:22:43 +0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -57,12 +57,12 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Pranav-Saraswat/Testing-Alpha",
+    "https://github.com/erslry/musisi",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 # GIT TOKEN ( if your edited repo is private)
-GIT_TOKEN = getenv("GIT_TOKEN", None)
+GIT_TOKEN = getenv("GIT_TOKEN", "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")
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
