4e976f6aef04bed2572cc218dc3ab9d54d934489
dim <147292243+erslry@users.noreply.github.com>
2023-11-20 22:05:03 +0800

    Update mute.py

--- YukkiMusic/plugins/admins/mute.py
+++ YukkiMusic/plugins/admins/mute.py
@@ -15,26 +15,26 @@ from config import BANNED_USERS
 from strings import get_command
 from YukkiMusic import app
 from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import is_muted, mute_on
+from YukkiMusic.utils.database import is_music_playing, music_off
 from YukkiMusic.utils.decorators import AdminRightsCheck
 
 # Commands
-MUTE_COMMAND = get_command("MUTE_COMMAND")
+PAUSE_COMMAND = get_command("PAUSE_COMMAND")
 
 
 @app.on_message(
-    filters.command(MUTE_COMMAND)
+    filters.command(PAUSE_COMMAND)
     & filters.group
     & ~BANNED_USERS
 )
 @AdminRightsCheck
-async def mute_admin(cli, message: Message, _, chat_id):
-    if len(message.command) != 1 or message.reply_to_message:
+async def pause_admin(cli, message: Message, _, chat_id):
+    if len(message.command) != 1:
         return await message.reply_text(_["general_2"])
-    if await is_muted(chat_id):
-        return await message.reply_text(_["admin_5"])
-    await mute_on(chat_id)
-    await Yukki.mute_stream(chat_id)
+    if not await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_1"])
+    await music_off(chat_id)
+    await Yukki.pause_stream(chat_id)
     await message.reply_text(
-        _["admin_6"].format(message.from_user.mention)
+        _["admin_2"].format(message.from_user.mention)
     )
